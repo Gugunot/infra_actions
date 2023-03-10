f010aa458a5000e0c51af0dadadd7e61577384a1
Андрей Белухин <light42@yandex.ru>
2023-03-10 17:39:30 +0300

    change settings.py

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('I will be developer!')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
